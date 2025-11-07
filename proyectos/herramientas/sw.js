d0cb80dae72ff6d676b369cf159f141719214910
AlexRamSan <ramirez.miguel.alejandro@gmail.com>
2025-11-07 12:22:35 -0600

    Create sw.js

---
+++ proyectos/herramientas/sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'alexrasa-tools-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './assets/css/styles.css',
+  './assets/js/app.js',
+  './assets/js/svgTool.js',
+  './assets/js/dxf.js',
+  './assets/js/webglPreview.js',
+  './assets/js/presets.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  const isHTML = req.headers.get('accept')?.includes('text/html');
+  if (isHTML) {
+    e.respondWith(
+      fetch(req).then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return r;
+      }).catch(() => caches.match(req))
+    );
+  } else {
+    e.respondWith(caches.match(req).then(hit => hit || fetch(req)));
+  }
+});
