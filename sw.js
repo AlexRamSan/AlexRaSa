6c62b8134bc1a244605e6b970289413a0254c2c4
AlexRamSan <ramirez.miguel.alejandro@gmail.com>
2026-05-18 22:16:48 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Service Worker mínimo requerido para que iOS reconozca la PWA
+self.addEventListener('fetch', function(event) {
+    // Aquí se podría configurar el caché offline en el futuro
+});
